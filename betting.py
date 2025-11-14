919a6be61094aef94484ee14a657d746e263669e
CadenChau0 <126932621+CadenChau0@users.noreply.github.com>
2025-11-14 08:58:52 -0500

    Update betting.py

--- betting.py
+++ betting.py
@@ -4,7 +4,8 @@ import pandas as pd
 
 df = pd.read_csv("bets.csv", sep=";")
 
-# Compute winnings per sport (sum of gain)
 winnings_by_sport = df.groupby("sport")["gain"].sum().reset_index()
+
+winnings_by_sport = winnings_by_sport.sort_values(by="gain", ascending=False)
 
 winnings_by_sport.to_json("winnings.json", orient="records")
